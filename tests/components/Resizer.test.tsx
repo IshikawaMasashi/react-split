fc2661d66937fa03ea0dcd7ec962c114bae4d579
雅史 石川 <a622740@gmail.com>
2019-08-14 18:52:29 +0900

    修正

---
+++ tests/components/Resizer.test.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import { Resizer } from "../../src/components/Resizer";
+// import { styles } from "../../src/utils/styles";
+import { SplitOrientation } from "../../src/components/Split";
+
+describe("Resizer.test", () => {
+  it("Horizontal class test", () => {
+    const component = shallow(
+      <Resizer
+        orientation={SplitOrientation.Horizontal}
+        onMouseDown={event => {}}
+      />
+    );
+
+    // expect(
+    //   component.contains(
+    //     <div className={"resizer horizontal"} onMouseDown={event => {}} />
+    //   )
+    // ).toBe(true);
+    expect(component.hasClass("horizontal")).toBe(true);
+  });
+});
