12f46a7868d8bb06b016646223d0de09f9af0484
雅史 石川 <a622740@gmail.com>
2020-04-23 08:30:55 +0900

    Fix

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import Split from './components/Split';
 export { SplitOrientation } from './components/Split';
 export { default as useSplits } from './hooks/useSplits';
 export { SplitInfo } from './components/Split';
-export { default as SplitView } from './components/SplitView';
+export { default as SplitView, SplitViewRef } from './components/SplitView';
 
 export default Split;
